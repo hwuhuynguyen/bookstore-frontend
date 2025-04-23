05e3cb6d76eecb4e09ba1f066ecd3a472e38d041
Huy Nguyễn Hữu <nguyenhuuhuya1@gmail.com>
2025-04-23 23:00:51 +0700

    feat: setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MantineProvider } from "@mantine/core";
 import AppRoutes from "./routes";
 import "@mantine/core/styles.css";
-import "./App.css";
 
 function App() {
   const queryClient = new QueryClient();
